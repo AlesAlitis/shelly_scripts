bfa76cde7440d025ddbb9706def4dd557777f616
Nedyalko Vasilev <13307869+AlesAlitis@users.noreply.github.com>
2023-06-08 09:21:21 +0200

    Code streamlined to reduce resource usage. Added a check to turn the switch on when moving to detached mode

--- shelly_1_plus/change_mode_on_ha_availability.js
+++ shelly_1_plus/change_mode_on_ha_availability.js
@@ -1,33 +1,24 @@
 // If firmware version is lower than 1, relace const with let
-const outputId = 0; // For Shelly 1 Plus it's always 0. Change if needed
+const switchId = 0; // For Shelly 1 Plus it's always 0. Change if needed
 const checkPeriod = 10 * 1000; // in ms
-const hsUrl = 'http://YOUR_HOME_ASSISTAN_URL_OR_IP:8123';
-const operationModes = {
-  haControlled: "detached",
-  switchControlled: "flip" // Could be "follow" or "momentary" too
-}
-
-// "id" is the ID of the output to which the new configuration will be applied and "config" is the actual configuration
-const config = {
-  id: outputId,
-  config: {
-    in_mode: operationModes.haControlled
-  }
-};
-
-let currentMode = operationModes.haControlled;
+const haUrl = 'http://YOUR_HOME_ASSISTAN_URL_OR_IP:8123'; // As sugested by bvhme
+let mode = "detached"; // Could be "detached", "flip", "follow" or "momentary"
 
 function updateConfig(oldConfig) {
   //Aplly the new config only if the switch is not already working in the desired mode  
-  if (oldConfig.in_mode !== currentMode) Shelly.call("Switch.SetConfig", config);
+  if (oldConfig.in_mode !== mode) {
+    Shelly.call("Switch.SetConfig", { id: switchId, config: { in_mode: mode } });
+    // If moving to HA controlled mode, make sure that the relay is switched on
+    if (mode === "detached") Shelly.call("Switch.Set", { id: switchId, on: true });
+  }
 }
 
-function setSwitchFromRespons(resp) {
+function setSwitchFromRespons(response) {
   // If HA is accessible the response code is 200 update the current mode and the new configuration
-  currentMode = config.config.in_mode = (resp.code === 200)? operationModes.haControlled : operationModes.switchControlled;
+  mode = (response.code === 200)? "detached" : "flip";
   
   // Get the current output configuration and pass it to updateConfig
-  Shelly.call("Switch.GetConfig", {id: outputId}, updateConfig)
+  Shelly.call("Switch.GetConfig", {id: switchId}, updateConfig)
 }
 
 function testHA() {
