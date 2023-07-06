bbda07cd2672a6a3cc2bd8c76500996f692d869f
Nedyalko Vasilev <13307869+AlesAlitis@users.noreply.github.com>
2023-07-06 20:36:58 +0200

    refactor to 1 function to stop script from crashing

--- shelly_1_plus/change_mode_on_ha_availability.js
+++ shelly_1_plus/change_mode_on_ha_availability.js
@@ -2,29 +2,23 @@
 const switchId = 0; // For Shelly 1 Plus it's always 0. Change if needed
 const checkPeriod = 10 * 1000; // in ms
 const haUrl = 'http://YOUR_HOME_ASSISTAN_URL_OR_IP:8123'; // As sugested by bvhme
-let mode = "detached"; // Could be "detached", "flip", "follow" or "momentary". Use "detached" when HA is in control
 
-function updateConfig(oldConfig) {
-  //Aplly the new config only if the switch is not already working in the desired mode  
-  if (oldConfig.in_mode !== mode) {
-    Shelly.call("Switch.SetConfig", { id: switchId, config: { in_mode: mode } });
-    // If moving to HA controlled mode, make sure that the relay is switched on
-    if (mode === "detached") Shelly.call("Switch.Set", { id: switchId, on: true });
-  }
+// For RPC command HTTP.GET, both url and body are required. 
+function testHA() { Shelly.call("HTTP.GET", {url: haUrl, body:{}}, function(resp) {
+    // If HA is accessible the response code is 200 update the current mode and the new configuration
+    // Could be "detached", "flip", "follow" or "momentary". Use "detached" when HA is in control
+    let mode = (resp && resp.code === 200)? "detached" : "flip"; 
+
+    // Get the current output configuration and pass it to updateConfig
+    Shelly.call("Switch.GetConfig", {id: switchId}, function(oldConfig) {
+      //Aplly the new config only if the switch is not already working in the desired mode
+      if (oldConfig.in_mode !== mode) {
+        Shelly.call("Switch.SetConfig", { id: switchId, config: { in_mode: mode } });
+        // If moving to HA controlled mode, make sure that the relay is switched on
+        if (mode === "detached") Shelly.call("Switch.Set", { id: switchId, on: true });
+      }
+    });
+  })
 }
 
-function setSwitchFromRespons(response) {
-  // If HA is accessible the response code is 200 update the current mode and the new configuration
-  mode = (response.code === 200)? "detached" : "flip";
-  
-  // Get the current output configuration and pass it to updateConfig
-  Shelly.call("Switch.GetConfig", {id: switchId}, updateConfig)
-}
-
-function testHA() {
-  // For RPC command HTTP.GET, both url and body are required. 
-  Shelly.call("HTTP.GET", {url: haUrl, body:{}}, setSwitchFromRespons);
-}
-
-// Timer.set(period_in_ms, reapeate_indefinitely, function)
 Timer.set(checkPeriod, true, testHA);
